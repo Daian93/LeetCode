7729b5b5b50506ebb9b51e2f8d9bca179f55e2d4
Diana Rammal Sansón <104791057+Daian93@users.noreply.github.com>
2025-08-29 00:51:56 +0200

    Time: 0 ms (100.00%) | Memory: 20 MB (73.74%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.swift
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.swift
@@ -3,10 +3,11 @@ class Solution {
         var n = 1
 
         for i in 1..<nums.count {
-            if nums[i] != nums[i - 1] {
-                nums[n] = nums[i]
-                n += 1
+            if nums[i] == nums[i - 1] {
+                continue
             }
+            nums[n] = nums[i]
+            n += 1
         }
 
         return n
