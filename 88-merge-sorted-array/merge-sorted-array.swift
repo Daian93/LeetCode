bea1eb1bcbd2529f17c14cb51a2f53fb0edcc379
Diana Rammal Sansón <104791057+Daian93@users.noreply.github.com>
2025-08-28 19:09:52 +0200

    Update merge-sorted-array.swift

--- 88-merge-sorted-array/merge-sorted-array.swift
+++ 88-merge-sorted-array/merge-sorted-array.swift
@@ -5,21 +5,14 @@ class Solution {
         var p = m + n - 1  // nums1 last index
 
         // Merge in reverse order
-        while p1 >= 0 && p2 >= 0 {
-            if nums1[p1] > nums2[p2] {
+        while p2 >= 0 {
+            if p1 >= 0 && nums1[p1] > nums2[p2] {
                 nums1[p] = nums1[p1]
                 p1 -= 1
             } else {
                 nums1[p] = nums2[p2]
                 p2 -= 1
             }
-            p -= 1
-        }
-
-        // Fill nums1 with leftover nums2 elements
-        while p2 >= 0 {
-            nums1[p] = nums2[p2]
-            p2 -= 1
             p -= 1
         }
     }
