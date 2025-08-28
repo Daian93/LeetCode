fb17c3b8aabfa53b1f5719d7fca00bc88d694b2e
Diana Rammal Sansón <104791057+Daian93@users.noreply.github.com>
2025-08-29 00:50:49 +0200

    Time: 0 ms (100.00%) | Memory: 19.8 MB (83.58%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        var n = 1
+
+        for i in 1..<nums.count {
+            if nums[i] != nums[i - 1] {
+                nums[n] = nums[i]
+                n += 1
+            }
+        }
+
+        return n
+    }
+}
