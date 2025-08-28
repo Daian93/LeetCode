d73d67bfcd576b0f86dbdf217fc81c2c9f679ccb
Diana Rammal Sansón <104791057+Daian93@users.noreply.github.com>
2025-08-28 19:09:33 +0200

    Update merge-sorted-array-explanation.swift

--- 88-merge-sorted-array/merge-sorted-array-explanation.swift
+++ 88-merge-sorted-array/merge-sorted-array-explanation.swift
@@ -35,21 +35,14 @@ class Solution {
         y el puntero p. Repetimos este paso hasta que uno de los arrays se quede
         sin elementos pendientes.
         */
-        while p1 >= 0 && p2 >= 0 {
-            if nums1[p1] > nums2[p2] {
+        while p2 >= 0 {
+            if p1 >= 0 && nums1[p1] > nums2[p2] {
                 nums1[p] = nums1[p1]
                 p1 -= 1
             } else {
                 nums1[p] = nums2[p2]
                 p2 -= 1
             }
-            p -= 1
-        }
-
-        // Si quedan elementos en nums2, los copiamos.
-        while p2 >= 0 {
-        	nums1[p] = nums2[p2]
-            p2 -= 1
             p -= 1
         }
     }
