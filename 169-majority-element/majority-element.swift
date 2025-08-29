86f6c861858f43112389b3def8337bd969c87a88
Diana Rammal Sansón <104791057+Daian93@users.noreply.github.com>
2025-08-29 17:07:02 +0200

    Time: 0 ms (100.00%) | Memory: 19.3 MB (92.41%) - LeetSync

---
+++ 169-majority-element/majority-element.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        var candidate = nums[0]
+        var count = 0
+        
+        for num in nums {
+            if count == 0 {
+                candidate = num
+            }
+            count += (num == candidate) ? 1 : -1
+        }
+        
+        return candidate
+    }
+}
